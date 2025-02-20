d14cd5fc25a6b9ab9c5cdf2d836c6d255b37d59a
nestor-remo <nestiray12345@gmail.com>
2025-02-19 20:26:31 -0800

    set h1 general font

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const About = () => {
     return (
-        <div className='About'>
+        <div>
             <h1> Hello! I'm <span>Nestor</span> Reategui </h1>
             <p> I'm an undergraduate Computer Science major with a Mathematics minor at California State University - Fullerton</p>
             <h1> I'm currently working on this site so it's not completely ready </h1>
